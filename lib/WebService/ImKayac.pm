379f0475418533ba897ba2b70629adeea2429b4a
Taiyoh Tanaka <sun.basix@gmail.com>
2010-02-19 19:47:28 +0900

    initial commit

---
+++ lib/WebService/ImKayac.pm
@@ -0,0 +1,33 @@
+package WebService::ImKayac;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::ImKayac -
+
+=head1 SYNOPSIS
+
+  use WebService::ImKayac;
+
+=head1 DESCRIPTION
+
+WebService::ImKayac is
+
+=head1 AUTHOR
+
+taiyoh E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
